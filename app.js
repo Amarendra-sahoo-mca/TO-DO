1c3b85715406bc7e7f90a7397578872eea56c1ef
Amarendra-sahoo-mca <amarendrasahoo521@gmail.com>
2024-02-28 17:49:33 +0530

    100% complited

---
+++ app.js
@@ -0,0 +1,37 @@
+let inp=document.querySelector("input");
+let btn1=document.querySelector("#btn1");
+let btn2=document.querySelector("#btn2");
+let ol=document.querySelector("ol");
+
+btn1.addEventListener("click", function(){
+    let li=document.createElement("li");
+    let dBtn=document.createElement("button");
+    dBtn.innerText="X";
+    dBtn.classList.add("x");
+    li.classList.add("y");
+    li.innerText=inp.value;
+    li.appendChild(dBtn);
+    ol.appendChild(li);
+    inp.value=null;
+});
+btn2.addEventListener("click", function(){
+    let li=document.createElement("li");
+    let dBtn=document.createElement("button");
+    dBtn.innerText="X";
+    dBtn.classList.add("x");
+    li.classList.add("y");
+    li.innerText=inp.value;
+    li.appendChild(dBtn);
+    ol.prepend(li);
+    inp.value=null;
+});
+ol.addEventListener("click", function(event){
+    if(event.target.nodeName=="BUTTON"){
+        let pr=event.target.parentElement;
+        pr.remove();
+    }
+    
+    
+})
+
+
