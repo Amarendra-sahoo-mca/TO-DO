43041acfc21b4a677ae1e4ba4a01252e08019aa7
Amarendra-sahoo-mca <amarendrasahoo521@gmail.com>
2024-03-05 20:23:29 +0530

    little update

--- app.js
+++ app.js
@@ -14,24 +14,24 @@ btn1.addEventListener("click", function(){
     ol.appendChild(li);
     inp.value=null;
 });
-btn2.addEventListener("click", function(){
-    let li=document.createElement("li");
-    let dBtn=document.createElement("button");
-    dBtn.innerText="X";
-    dBtn.classList.add("x");
-    li.classList.add("y");
-    li.innerText=inp.value;
-    li.appendChild(dBtn);
-    ol.prepend(li);
-    inp.value=null;
+inp.addEventListener('keyup', (key)=>{
+    if(key.keyCode === 13){
+        let li=document.createElement("li");
+        let dBtn=document.createElement("button");
+        dBtn.innerText="X";
+        dBtn.classList.add("x");
+        li.classList.add("y");
+        li.innerText=inp.value;
+        li.appendChild(dBtn);
+        ol.appendChild(li);
+        inp.value=null;
+    }
 });
 ol.addEventListener("click", function(event){
     if(event.target.nodeName=="BUTTON"){
         let pr=event.target.parentElement;
         pr.remove();
     }
-    
-    
 })
 
 
